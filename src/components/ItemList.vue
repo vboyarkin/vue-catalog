aa5cf3cfe19e41b2922e55064ca9a990b2dbd5ad
Vladislav Boyarkin <boyarkinvladislavv@gmail.com>
2022-01-17 15:11:33 +0300

    Add loader

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,16 +1,38 @@
 <template>
-  <div class="item-list">
-    <ItemCard class="product-card" v-for="item of items" :key="item.id" :item="item"/>
+  <div>
+    <div v-if="!isFetching && items && items.length !== 0" class="item-list">
+      <ItemCard
+        class="product-card"
+        v-for="item of items"
+        :key="item.id"
+        :item="item"
+      />
+    </div>
+
+    <div
+      v-if="!isFetching && !(items && items.length !== 0)"
+      class="item-list-placeholder"
+    >
+      <span>Нет подходящих товаров</span>
+    </div>
+
+    <div v-if="isFetching" class="item-list-loader-placeholder">
+      <span>Загрузка</span>
+    </div>
   </div>
 </template>
 
 <script>
-import ItemCard from './ItemCard.vue';
+import ItemCard from "./ItemCard.vue";
 export default {
   components: { ItemCard },
   props: {
     items: {
       type: Array,
+      required: true,
+    },
+    isFetching: {
+      type: Boolean,
       required: true,
     },
   },
@@ -27,4 +49,26 @@ export default {
   margin: 20px 44px
   width: 200px
   height: 330px
+
+.item-list-placeholder, .item-list-loader-placeholder
+  width: 100%
+  display: flex
+  justify-content: center
+  align-items: center
+
+  span
+    font-size: 2.5rem
+    color: $color-active
+
+.item-list-loader-placeholder > span
+  animation: pulse 1s linear infinite
+
+@keyframes pulse
+  from
+    opacity: 100%
+  to
+    opacity: 1000%
+
+  50%
+    opacity: 25%
 </style>
